c466cf8c0d113e750fe0ebf91c07bb18faeba541
davidigorre <david_igorre@hotmail.com>
2016-06-21 23:55:30 +0200

    only .exe
    only .exe

--- cubePlatform/Assets/Scripts/Level/NextLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class NextLevel : MonoBehaviour {
-
-	void OnTriggerEnter(Collider col){
-		Application.LoadLevel("Menu");
-	}
-}
--- cubePlatform/Assets/Scripts/Sky/SkyRotation.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class SkyRotation : MonoBehaviour {
-
-	void FixedUpdate () {
-		GetComponent<Rigidbody>().angularVelocity = new Vector3(0,0,2) * Time.deltaTime ;
-	}
-}
